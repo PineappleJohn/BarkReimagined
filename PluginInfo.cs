2db0c06a38603bb0916ccc77675f1a8ee8a29e85
John <forddarwin6@gmail.com>
2024-06-20 16:48:12 -0500

    Add files via upload
    release one ig

---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Bark
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.john.gorillatag.barkreimagined";
+        public const string Name = "BarkReimagined";
+        public const string Version = "1.0.0"; 
+    }
+}
